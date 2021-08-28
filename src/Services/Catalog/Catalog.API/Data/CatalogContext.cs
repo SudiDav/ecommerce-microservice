bf43125a6c9aee443cc296af7e404e5b37c84dbd
Sudi Simba <sudisimbadav@gmail.com>
2021-08-28 11:40:02 +0200

    Completed the Product Interface

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration _config)
+        {
+            var client = new MongoClient(_config.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(_config.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(_config.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
